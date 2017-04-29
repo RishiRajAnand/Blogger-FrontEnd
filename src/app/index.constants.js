5afafe2e96ac8a7a0bdfab4d0c08c3bbc5c788f3
Mona Lisa <rishiraj@signzy.com>
2017-04-30 00:24:43 +0530

    project setup

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('bloggerFrontEnd')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
